4bb3f4b02b8977719e3eb122bd38ce8fea2188f4
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2023-12-02 15:35:00 -0300

    refactor: move styles to tailwind layers

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,3 @@
-import Input from "../Input/Input";
-
 const Form = ({ onSubmit }) => {
   const safeSubmit = (event) => {
     event.preventDefault();
@@ -14,12 +12,19 @@ const Form = ({ onSubmit }) => {
       onSubmit={safeSubmit}
       className="h-full w-full flex flex-col items-center justify-center gap-10 -mt-20 mx-5"
     >
-      <Input type="text" required placeholder="Insira seu nome..." />
-      <Input type="email" required placeholder="Insira seu e-mail..." />
-      <button
-        type="submit"
-        className="py-1 px-5 w-full max-w-sm bg-alura-100 dark:bg-alura-200 rounded-full text-gray-200 uppercase outline-none hover:animate-pulse hover:shadow-md hover:shadow-gray-500 hover:dark:shadow-black"
-      >
+      <input
+        className="alura-input"
+        type="text"
+        required
+        placeholder="Insira seu nome..."
+      />
+      <input
+        className="alura-input"
+        type="email"
+        required
+        placeholder="Insira seu e-mail..."
+      />
+      <button type="submit" className="alura-button">
         Seguir
       </button>
     </form>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,7 @@ const Header = ({ user }) => {
   return (
     <div className="p-5 w-full z-50 sm:m-5 sm:rounded-xl bg-alura-200 dark:bg-dark-200 flex justify-between items-center">
       <span className="text-gray-100">Olá, {user?.name || "Usuário"}</span>
-      <h1 className="text-gray-100 text-xl sm:hover:text-2xl sm:cursor-pointer">
-        Alura Newsletter
-      </h1>
+      <h1>Alura Newsletter</h1>
       <ToggleTheme />
     </div>
   );
