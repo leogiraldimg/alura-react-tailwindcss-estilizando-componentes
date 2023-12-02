ada9c84d7a82541f11c216acadcf26124c479870
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2023-12-02 15:54:09 -0300

    feat: add some plugins

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -13,9 +13,12 @@ const Article = ({ title, text, tags, image, alt }) => {
       </div>
       <div className="flex flex-col gap-1">
         {text.map((content, index) => (
-          <span key={index} className="text-alura-200 dark:text-gray-400">
+          <p
+            key={index}
+            className="text-alura-200 dark:text-gray-400 line-clamp-2 sm:line-clamp-none"
+          >
             {content}
-          </span>
+          </p>
         ))}
       </div>
       {image && <img className="sm:p-4" src={image} />}
